34ed5a133d6c90597744dfa705a92c896bd6ecbc
nikito-error <nisanx@abv.bg>
2022-10-01 11:19:23 +0300

    update-ready

--- src/main/java/com/em/system/controller/EmployeeController.java
+++ src/main/java/com/em/system/controller/EmployeeController.java
@@ -52,7 +52,7 @@ public class EmployeeController {
 		employee.setFirstName(employeeDetails.getFirstName());
 		employee.setLastName(employeeDetails.getLastName());
 		employee.setEmail(employeeDetails.getEmail());
-		Employee updateEmployee = employeeRepository.save(employee);
-		return ResponseEntity.ok(updateEmployee);
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
 	}
 }
